dd658e8520ebbe53896bcf918b6176a6cc867481
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-04 12:21:27 -0600

    VB Arreglo de positivo y negativo

---
+++ Arrelo de positivo y negativo/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        'Declaramos las variables de tipo entero
+        'Arreglo de 6 posiciones
+        'Array unidimensional
+        Dim Arreglo(6) As Integer
+        Dim Suma_positivos, Suma_negativos As Integer
+        Console.WriteLine("Ingrese los números")
+        'Ciclo for que recorre el la misma cantidad
+        'de posicioes que tiene el arreglo
+        For i = 1 To 6
+            'Mensaje que muestra la posición del ciclo for
+            Console.WriteLine("Numero" & i)
+            'Método que obtiene los datos ingresados 
+            'y lo almacena en el arreglo
+            Arreglo(i) = Convert.ToInt32(Console.ReadLine())
+            'Condición que evalúa el arreglo
+            If Arreglo(i) > 0 Then
+                'Operador de asignación += el significado 
+                'Suma_Positivos = Suma_Positivos + Arreglo
+                Suma_positivos += Arreglo(i)
+            Else
+                Suma_negativos += Arreglo(i)
+            End If
+        Next
+        Console.WriteLine("La suma de positivos es : " & Suma_positivos)
+        Console.WriteLine("La suma de negativos es : " & Suma_negativos)
+        Console.ReadLine()
+    End Sub
+
+End Module
